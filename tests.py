d2f54e08012855419a30ea5ed78de8c7c7df9aea
MARINA ZHUKOVA <marishka19882007@yandex.ru>
2025-05-31 10:30:21 +0300

    Глобальные исправления в Финальную работу Спринта 4

--- tests.py
+++ tests.py
@@ -1,51 +1,64 @@
+import pytest
+
+
 class TestBooksCollector:
 
-#Проверка попытки добавить книгу с длинным названием (>40 символов)
-    def test_add_new_book_no_more_than_40(self, books_collector):
-        books_collector.add_new_book('Прежде, чем я упаду')
-        assert len(self.name) > 40
+#Проверка попытки добавить книгу
+    @pytest.mark.parametrize('book', ['Лолита', 'Азазель'])
+    def test_add_new_book_positive(self, books_collector, book):
+        books_collector.add_new_book(book)
+        assert books_collector.books_genre[book] == ''
 
 #Проверка, что метод возвращает правильный жанр, если книга найдена
-    def test_set_book_genre_is_selected_from_list(self, books_collector):
-        assert books_collector.books_genre['Крепкий орешек'] == 'Детектив'
+    def test_set_book_genre_valid_name(self, books_collector):
+        books_collector.add_new_book('Крепкий орешек')
+        books_collector.set_book_genre('Крепкий орешек', 'Детективы')
+        assert books_collector.books_genre['Крепкий орешек'] == 'Детективы'
 
-#Проверка метода get_books_with_specific_genre на получение правильных результатов.
-    def test_get_book_genre_found(self, books_collector):
-        genre = books_collector.get_book_genre("Дюна")
-        assert genre == "Фантастика"
+#Проверка успешного возврата жанра книг
+    def test_get_book_genre_existing(self, books_collector):
+        books_collector.add_new_book('Мастер и Маргарита')
+        books_collector.set_book_genre('Мастер и Маргарита', 'Фантастика')
+        assert books_collector.get_book_genre('Мастер и Маргарита') == 'Фантастика'
 
 #Проверка получения книг жанра 'Фантастика
-    def test_get_books_with_specific_genre_fantasy(self, books_collector):
-        fantasy_books = books_collector.get_books_with_specific_genre("Фантастика")
-        assert fantasy_books == ["Дюна", "Пятый элемент"]
+    def test_get_books_with_specific_genre_when_valid_genre(self, books_collector):
+        books_collector.add_new_book('Дюна')
+        books_collector.set_book_genre('Дюна', 'Фантастика')
+        assert books_collector.get_books_with_specific_genre('Фантастика')  \
+               and type(books_collector.get_books_with_specific_genre('Фантастика')) == list
 
-#Проверяет, что метод get_books_genre возвращает точный словарь books_genre    
-    def test_get_books_genre_current_list(self, books_genre):
-        expected_books_genre = kb.books_genre
-        actual_books_genre = books_genre.get_books_genre()
-        assert actual_books_genre == expected_books_genre
+#Проверяет, что метод get_books_genre возвращает пустой словарь books_genre    
+    def test_get_books_genre_empty_dict(self, books_collector):
+        assert not books_collector.get_books_genre()
 
 #Проверка позитивного сценария — отбор детских книг    
-    def test_get_books_for_children_only_children_books(self, kids_book):
-        expected_books = kb.books_genre
-        children_books = kids_book.get_books_for_children()       
-        assert children_books == expected_books
+    def test_get_books_for_children_correct_genre(self, books_collector):
+        books = ['Золотой ключик', 'Русалочка', 'Малыш и Карлсон']
+        x = 0
+        for name in books:
+            books_collector.add_new_book(name)
+            books_collector.set_book_genre(name, books_collector.genre[x])
+            x += 1
+
+        for rating in books_collector.genre_age_rating:
+            assert rating not in books_collector.get_books_for_children()
 
 #Проверка положительного сценария добавления книги в избранные
-    def test_add_book_in_favorites_success(self, books_genre):
-        books_genre.add_book_in_favorites("Война миров")
-        assert "Война миров" in books_genre.favorites
+    def test_add_book_in_favorites_success(self, books_collector):
+        books_collector.add_new_book('Сонная лощина')
+        books_collector.add_book_in_favorites('Сонная лощина')
+        assert 'Сонная лощина' in books_collector.favorites
 
 #проверка успешного удаления книги из избранного
-    def test_delete_book_from_favorites_deletion_completed(self, favorites_books):
-        favorites_books.delete_book_from_favorites("Такси")
-        assert "Вино из одуванчиков" not in favorites_books.favorites
+    def test_delete_book_from_favorites_deletion_completed(self, books_collector):
+        books_collector.delete_book_from_favorites("Такси")
+        assert "Вино из одуванчиков" not in books_collector.favorites
 
-#Проверка возвращения списка избранных книг, когда список не пуст.
-    def test_get_list_of_favorites_books_return_favorites(self, favorites_books):
-        favorite_book = favorites_books.get_list_of_favorites_books()
-        assert favorite_book == ["Вино из одуванчиков", "Такси"]
-
-@pytest.mark.parametrize('fav_books', books_genre)
-def test_check_fav_books(books_genre):
-    assert check_fav_books(books_genre)
+#Проверка возвращения списка избранных книг
+    def test_get_list_of_favorites_books_not_empty(self, books_collector):
+        books = ['1984', 'Заводной апельсин', 'Мы']
+        for name in books:
+            books_collector.add_new_book(name)
+            books_collector.add_book_in_favorites(name)
+        assert books_collector.get_list_of_favorites_books()
